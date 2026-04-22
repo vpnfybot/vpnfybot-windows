745b29dc5ad66ac6d87254403d0744bf48b76e4e
pinqtape <vpnfybot@gmail.com>
2026-04-23 04:47:37 +0800

    Optimizations & module structure
    Optimizations & module structure

---
+++ src/target/release/build/wg-gui-f12dd93e0d2f4220/build_script_build-f12dd93e0d2f4220.d
@@ -0,0 +1,5 @@
+C:\Users\vayzer\Desktop\vpnfybot-windows\src\target\release\build\wg-gui-f12dd93e0d2f4220\build_script_build-f12dd93e0d2f4220.d: build.rs
+
+C:\Users\vayzer\Desktop\vpnfybot-windows\src\target\release\build\wg-gui-f12dd93e0d2f4220\build_script_build-f12dd93e0d2f4220.exe: build.rs
+
+build.rs:
